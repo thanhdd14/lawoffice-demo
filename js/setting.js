36499a4966650a74ff76d495cef72059889c0d83
thanhdd <duythanh14189@gmail.com>
2024-12-30 16:14:42 +0700

    up

---
+++ js/setting.js
@@ -0,0 +1,125 @@
+//menu header
+$('.js-mobile').on('click', function(){
+	$(this).toggleClass("js-mobile--close");
+	$("html").toggleClass("js-locked");
+	$("#header").toggleClass("active");
+	$(".header-nav").toggleClass("active");
+});
+$('.header-nav__menu-sub').on('click', function(){
+	$(this).toggleClass("active");
+	
+	$(this).next(".header-nav__submenu").slideToggle();
+});
+
+
+
+
+
+
+$('.mv-slider').slick({
+	dots: true,
+	focusOnSelect: true,
+	pauseOnHover:false,
+	infinite: true,
+	speed: 500,
+	fade: true,
+	autoplay: false,
+	cssEase: 'linear',
+});
+
+
+$('.js-attorney-slider').slick({
+	centerMode: true,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	responsive: [
+		{
+			breakpoint: 835,
+			settings: {
+				slidesToShow: 2,
+			}
+		},
+		{
+			breakpoint: 640,
+			settings: {
+				slidesToShow: 1,
+			}
+		}
+	]
+});
+$('.js-column-slider').slick({
+	
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	autoplay: false,
+	autoplaySpeed: 2000,
+	dots: true,
+	responsive: [
+		{
+			breakpoint: 835,
+			settings: {
+				slidesToShow: 2,
+				
+			}
+		},
+		{
+			breakpoint: 640,
+			settings: {
+				centerMode: true,
+				slidesToShow: 1,
+			}
+		}
+	]
+});
+
+
+if ($(window).width() < 835) {
+	var $status = $('.pagingInfo');
+	var $slickElement = $('.js-store-sp');
+
+	$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+		//currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+		var i = (currentSlide ? currentSlide : 0) + 1;
+		$status.text(i + '/' + slick.slideCount);
+	});
+
+	$slickElement.slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	});
+
+	$('.js-reason-sp').slick({
+		slidesToShow: 1,
+		centerMode: true,
+		slidesToScroll: 1,
+		infinite: false,
+	});
+}
+
+
+
+$(window).on("load",function(){
+	$(".c-table").mCustomScrollbar({
+		axis: "x", // horizontal scrollbar
+		theme:"dark"
+	});
+});
+
+
+// matchHeight
+jQuery(function ($) {
+	$('.business-reasons__list-item').matchHeight();
+	$('.column-list .column-list__item .column-list__box').matchHeight();
+	$(window).on("load resize", function () {
+		$('.business-reasons__list-item').matchHeight();
+		$('.column-list .column-list__item .column-list__box').matchHeight();
+	});
+});
+
+
+$('.view-plan').click(function () {
+	$(this).prev().toggleClass('is-show');
+	$(this).toggleClass('is-hide');
+});
